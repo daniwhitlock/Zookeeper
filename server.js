fc1f57b9ac72ec928dde184f7f2d11572e76d8dc
Fire Dragon <daniwhitlock4@gmail.com>
2021-01-30 07:24:50 -0700

    pushing small typo change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function filterByQuery(query, animalsArray) {
 
   // Note that we save the animalsArray as filteredResults here:
   let filteredResults = animalsArray;
-  
+
   if (query.personalityTraits) {
     // Save personalityTraits as a dedicated array.
     // If personalityTraits is a string, place it into a new array and save.
@@ -43,12 +43,26 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {
